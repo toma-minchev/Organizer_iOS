16fc9f832c917e47b9a05e7357b64a82615fc2e9
toma <toma.minchev@icloud.com>
2026-03-09 14:06:12 +0200

    starting on notifications

---
+++ Organizer/NotificationManager.swift
@@ -0,0 +1,16 @@
+import UserNotifications
+
+final class NotificationManager {
+    
+    static let shared = NotificationManager()
+    
+    func requestPermission() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if let error {
+                print("Notification permission error:", error)
+            }
+        }
+    }
+}
